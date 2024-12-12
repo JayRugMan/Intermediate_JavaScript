5501971c019efeceb5f27f24751128de705a8ab9
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-12-11 17:18:56 -0700

    format testing

--- sandbox/forms/public/main.js
+++ sandbox/forms/public/main.js
@@ -7,36 +7,51 @@ const submitButton = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-  
-	fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'apikey': apiKey
-    },
-    body: data
-  })
-  .then(response => {
-    if(response.ok){
-      return response.json();
-    }
-    throw new Error('Request failed!');
-  }, networkError => {console.log(networkError.message)})
-  .then(jsonResponse => {
-    renderResponse(jsonResponse);
-  })
+const showConfirmation = () => {
+  console.log('showConfirmation function called');
+
+  // Get the values from the inputs
+  const i1 = input1.value;
+  const i2 = input2.value;
+  const i3 = input3.value;
+  const i4 = input4.value;
+
+  // Create a list element
+  const ul = document.createElement('ul');
+  ul.id = 'responseList';
+
+  // Create a list item for each input
+  const confirmationItem1 = document.createElement('li');
+  confirmationItem1.textContent = `input1: ${i1}`;
+  ul.appendChild(confirmationItem1);
+
+  const confirmationItem2 = document.createElement('li');
+  confirmationItem2.textContent = `input2: ${i2}`;
+  ul.appendChild(confirmationItem2);
+
+  const confirmationItem3 = document.createElement('li');
+  confirmationItem3.textContent = `input3: ${i3}`;
+  ul.appendChild(confirmationItem3);
+
+  const confirmationItem4 = document.createElement('li');
+  confirmationItem4.textContent = `input1: ${i4}`;
+  ul.appendChild(confirmationItem4);
+
+  responseField.appendChild(ul);
 }
 
 // Clear page and call Asynchronous functions
 const displayEntries = (event) => {
   event.preventDefault();
+  console.log('Submit button clicked'); // Debugging log
+
+  // Clear response field
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
-  shortenUrl();
+
+  // Show replies for confirmation
+  showConfirmation();
 }
 
 submitButton.addEventListener('click', displayEntries);
